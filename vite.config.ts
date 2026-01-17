7748e27dd04e5daa720deb69d00c38ca8c4ab3c7
JeanDuken <jeandukendacius@gmail.com>
2026-01-17 04:13:31 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,5 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  
 }));
